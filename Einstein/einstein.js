b4a787d229e14bcf6b64af50584dd67f18bab50f
malmal200 <56348274+malmal200@users.noreply.github.com>
2019-10-12 14:09:51 +0100

    Add files via upload

---
+++ Einstein/einstein.js
@@ -0,0 +1,3 @@
+document.body.innerHTML += '<div id="sick-counter" style="position: fixed; top: 60px; right: 10px; z-index: 2000; padding: 10px;' +
+    ' border-radius: 3px; font-size: 25px; background: white none repeat scroll 0 0; margin: 2px; cursor: pointer; border: 3px solid red;">' +
+    'Healthy as a bean</div>';
