2e1c83f608acea61b5b566fbe370122fdb6efadd
malmal200 <56348274+malmal200@users.noreply.github.com>
2019-10-12 14:25:24 +0100

    Update einstein.js

--- Einstein/einstein.js
+++ Einstein/einstein.js
@@ -1,3 +1,5 @@
+let message = "Healthy as a bean";
+
 document.body.innerHTML += '<div id="sick-counter" style="position: fixed; top: 60px; right: 10px; z-index: 2000; padding: 10px;' +
     ' border-radius: 3px; font-size: 25px; background: white none repeat scroll 0 0; margin: 2px; cursor: pointer; border: 3px solid red;">' +
-    'Healthy as a bean</div>';
+    message + '</div>';
