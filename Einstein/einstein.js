37a331cebc1c80f7e1ecacde7ff9018dc20c6ab1
malmal200 <56348274+malmal200@users.noreply.github.com>
2019-10-25 19:06:26 +0100

    Update einstein.js

--- Einstein/einstein.js
+++ Einstein/einstein.js
@@ -2,7 +2,7 @@ let message = "Healthy as a bean!!!";
 let image = "https://ph-files.imgix.net/caf5608a-67ec-4f9f-acb5-db0052c33bed?auto=format"
 
 
-document.body.innerHTML += '<div id="sick-counter" style="position: fixed; top: 60px; right: 10px; z-index: 2000; padding: 10px;' +
+document.body.innerHTML += '<div style="position: fixed; top: 60px; right: 10px; z-index: 2000; padding: 10px;' +
     'border-radius:3px;font-size:25px; background: white none repeat scroll 0 0; margin: 2px; cursor: pointer; border: 3px solid red;">' +
     '<img src="' + image + '"> ' + message + '</img> </div>';
 
